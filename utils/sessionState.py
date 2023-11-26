6aad820a40b22ca5f2e3c19fad47855a305a3421
NeuralBeat! <robinklett33@gmail.com>
2023-11-26 11:31:38 +0100

    beautify

--- utils/sessionState.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-if 'selected_class' not in st.session_state:
-    st.session_state['selected_class'] = 'Select a class'
-
-if 'selected_subclass' not in st.session_state:
-    st.session_state['selected_subclass'] = None
