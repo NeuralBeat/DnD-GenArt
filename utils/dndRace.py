15c965d40ab4160594f29e16c8f70097ef3d59b2
NeuralBeat! <robinklett33@gmail.com>
2023-11-28 00:13:32 +0100

    added race image selection

--- utils/dndRace.py
+++ utils/dndRace.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import os
 
 def select_race():
     default_race = st.session_state.get('selected_race', '')
@@ -17,6 +18,21 @@ def select_race():
         'Orc': ['Half-Orc', 'Orc'],
         'Tiefling': ['Bloodline of Asmodeus', 'Bloodline of Mephistopheles', 'Bloodline of Zariel']
     }
+        # Define the path to your images folder
+    images_folder = 'images/races'
+
+    # Mapping of races to their images
+    race_images = {
+        'Dragonborn': 'dragonborn.webp',
+        'Dwarf': 'dwarf.webp',
+        'Elf': 'elf.webp',
+        'Genasi': 'genasi.webp',
+        'Gnome': 'gnome.webp',
+        'Halfling': 'halfling.webp',
+        'Human': 'human.webp',
+        'Orc': 'orc.webp',
+        'Tiefling': 'tiefling.webp'
+    }
 
         ##### INIT SESSION STATES ####
     if 'selected_race' not in st.session_state:
@@ -25,21 +41,34 @@ def select_race():
     if 'selected_subrace' not in st.session_state:
         st.session_state['selected_subrace'] = None
 
-        #### SESSION INTERNAL CLASS SELECTION
-    st.session_state['selected_race'] = st.selectbox(
-    'Select your Race', 
-    races, 
-    index=races.index(default_race),
-    key='race_selector'
-    )
+    # Use columns to split the layout
+    col1, col2 = st.columns(2)
 
-    # Update subclass state based on class selection
-    if st.session_state['selected_race'] in subraces:
-        st.session_state['selected_subrace'] = st.selectbox(
-            'Select your Subrace or Ancestry', 
-            subraces[st.session_state['selected_race']],
-            index=0 if default_subrace not in subraces[st.session_state['selected_race']] else subraces[st.session_state['selected_race']].index(default_subrace),
-            key='subrace_selector'
+    with col1:
+        #### SESSION INTERNAL CLASS SELECTION
+        st.session_state['selected_race'] = st.selectbox(
+        'Select your Race', 
+        races, 
+        index=races.index(default_race),
+        key='race_selector'
         )
-    else:
-        st.session_state['selected_subrace'] = ''
+
+        # Update subclass state based on class selection
+        if st.session_state['selected_race'] in subraces:
+            st.session_state['selected_subrace'] = st.selectbox(
+                'Select your Subrace or Ancestry', 
+                subraces[st.session_state['selected_race']],
+                index=0 if default_subrace not in subraces[st.session_state['selected_race']] else subraces[st.session_state['selected_race']].index(default_subrace),
+                key='subrace_selector'
+            )
+        else:
+            st.session_state['selected_subrace'] = ''
+
+        with col2:
+            # Create three columns for centering the image
+            spacer1, image_col, spacer2 = st.columns([1, 2, 1])
+            with image_col:
+                # Display image based on selected class
+                if st.session_state['selected_race'] in race_images:
+                    image_path = os.path.join(images_folder, race_images[st.session_state['selected_race']])
+                    st.image(image_path, caption=None, width=150, output_format='PNG')
