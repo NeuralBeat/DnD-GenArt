2205152a1e547abfb61c435c143f9574206a15ca
NeuralBeat! <robinklett33@gmail.com>
2023-12-10 13:58:16 +0100

    working draft: Attribute Handler

--- utils/attributeHandler.py
+++ utils/attributeHandler.py
@@ -1,5 +1,22 @@
 import streamlit as st
+from utils.dndClass import *
 
+class_primary_attributes = {
+    'Artificer': ['INTELLIGENCE', 'CONSTITUTION'],
+    'Barbarian': ['STRENGTH', 'CONSTITUTION'],
+    'Bard': ['CHARISMA', 'DEXTERITY'],
+    'Blood Hunter': ['DEXTERITY', 'INTELLIGENCE'],
+    'Cleric': ['WISDOM', 'CONSTITUTION'],
+    'Druid': ['WISDOM', 'INTELLIGENCE'],
+    'Fighter': ['STRENGTH', 'CONSTITUTION'],
+    'Monk': ['DEXTERITY', 'WISDOM'],
+    'Paladin': ['STRENGTH', 'CHARISMA'],
+    'Ranger': ['DEXTERITY', 'WISDOM'],
+    'Rogue': ['DEXTERITY', 'INTELLIGENCE'],
+    'Sorcerer': ['CHARISMA', 'CONSTITUTION'],
+    'Warlock': ['CHARISMA', 'CONSTITUTION'],
+    'Wizard': ['INTELLIGENCE', 'CONSTITUTION']
+}
 
 def increment_attribute(attr_name):
     point_cost = 2 if st.session_state[attr_name] >= 15 else 1
@@ -11,7 +28,7 @@ def decrement_attribute(attr_name):
     point_gain = 2 if st.session_state[attr_name] > 15 else 1
     if st.session_state[attr_name] > 6:
         st.session_state[attr_name] -= 1
-        st.session_state['available_points'] += point_gain
+        st.session_state['available_points'] += point_gain       
 
 def select_attributes():
     
@@ -21,63 +38,76 @@ def select_attributes():
     total_points = 27
     if 'available_points' not in st.session_state:
         st.session_state['available_points'] = total_points
+    
+    if 'apply_bonus' not in st.session_state:
+        st.session_state['apply_bonus'] = True  # Flag to control bonus application
 
     # Initialize attribute values in the session state
     for attr in attributes:
         if attr not in st.session_state:
             st.session_state[attr] = 8  # Default starting value
-
+    
     st.write(f"OPEN POINTS: {st.session_state['available_points']}")
 
     select_attribute_bonus()
 
     for attr in attributes:
         col1, col2, col3, col4 = st.columns([2, 1, 1, 1])
+        
         with col1:
             st.write(f"{attr}")
+        
         with col2:
-            st.button("-", key=f"minus_{attr}", on_click=decrement_attribute, args=(attr,))
+            st.button("DECREASE", key=f"minus_{attr}", on_click=decrement_attribute, args=(attr,))
+
         with col3:
             st.write(f"{st.session_state[attr]}")
-        with col4:
-            st.button("+", key=f"plus_{attr}", on_click=increment_attribute, args=(attr,))
 
-    st.info(f"MOST IMPORTANT STATS FOR {st.session_state['selected_class']} CLASS:")
+        with col4:
+            st.button("INCREASE", key=f"plus_{attr}", on_click=increment_attribute, args=(attr,))
+
+    if st.session_state['selected_class'] in class_primary_attributes:
+        primary_attrs = class_primary_attributes[st.session_state['selected_class']]
+        st.info(f"PRIMARY STATS FOR {st.session_state['selected_class']}: {', '.join(primary_attrs)}")
 
 def select_attribute_bonus():
-    # Option selection
-    bonus_option = st.radio("CHOOSE YOU ATTRIBUTE BONUS",
-                            ("+2/+1", "+1/+1/+1"),
+    bonus_option = st.radio("CHOOSE ATTRIBUTE BONUS",
+                            ("+2/+1", "+1/+1/+1"), horizontal=True,
                             key="racial_bonus_option")
-
-    # Initialize session state for bonus choices
-    if 'bonus_choice_1' not in st.session_state:
-        st.session_state['bonus_choice_1'] = 'STRENGTH'
-    if 'bonus_choice_2' not in st.session_state:
-        st.session_state['bonus_choice_2'] = 'DEXTERITY'
-    if 'bonus_choice_3' not in st.session_state:
-        st.session_state['bonus_choice_3'] = 'CONSTITUTION'
 
     attributes = ['STRENGTH', 'DEXTERITY', 'CONSTITUTION', 'INTELLIGENCE', 'WISDOM', 'CHARISMA']
 
+    display_bonus_selection(attributes, bonus_option)
+
+    # Apply bonuses if they haven't been applied yet or if the option has changed
+    if 'bonuses_applied' not in st.session_state or not st.session_state['bonuses_applied'] or st.session_state['previous_bonus_option'] != bonus_option:
+        apply_racial_bonus(attributes, bonus_option)
+        st.session_state['previous_bonus_option'] = bonus_option
+
+def reset_attributes_to_base(attributes):
+    for attr in attributes:
+        st.session_state[attr] = 8  # Reset to base value
+
+def display_bonus_selection(attributes, bonus_option):
     if bonus_option == "+2/+1":
         st.session_state['bonus_choice_1'] = st.selectbox("CHOOSE ATTRIBUTE FOR +2 BONUS", attributes, key='bonus_1')
         st.session_state['bonus_choice_2'] = st.selectbox("CHOOSE ATTRIBUTE FOR +1 BONUS", attributes, key='bonus_2')
-
-        # Apply bonuses
-        for attr in attributes:
-            st.session_state[attr] = 8  # Reset to base value
-        st.session_state[st.session_state['bonus_choice_1']] += 2
-        st.session_state[st.session_state['bonus_choice_2']] += 1
-
     elif bonus_option == "+1/+1/+1":
         st.session_state['bonus_choice_1'] = st.selectbox("FIRST ATTRIBUTE FOR +1 BONUS", attributes, key='bonus_3')
         st.session_state['bonus_choice_2'] = st.selectbox("SECOND ATTRIBUTE FOR +1 BONUS", attributes, key='bonus_4')
         st.session_state['bonus_choice_3'] = st.selectbox("THIRD ATTRIBUTE FOR +1 BONUS", attributes, key='bonus_5')
 
-        # Apply bonuses
-        for attr in attributes:
-            st.session_state[attr] = 8  # Reset to base value
+def apply_racial_bonus(attributes, bonus_option):
+    # Reset attributes to base value
+    #reset_attributes_to_base(attributes)
+    # Apply the bonuses
+    if bonus_option == "+2/+1":
+        st.session_state[st.session_state['bonus_choice_1']] += 2
+        st.session_state[st.session_state['bonus_choice_2']] += 1
+    elif bonus_option == "+1/+1/+1":
         st.session_state[st.session_state['bonus_choice_1']] += 1
         st.session_state[st.session_state['bonus_choice_2']] += 1
         st.session_state[st.session_state['bonus_choice_3']] += 1
+    
+    # Update the flag to indicate bonuses have been applied
+    st.session_state['bonuses_applied'] = True
