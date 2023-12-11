29760c150663abc19b0c31b447062cbb731a160a
NeuralBeat! <robinklett33@gmail.com>
2023-12-11 23:35:26 +0100

    Major Bugfix and roster selection

--- Roster.py
+++ Roster.py
@@ -15,6 +15,12 @@ def Roster():
     if 'current_portrait_index' not in st.session_state:
         st.session_state['current_portrait_index'] = 0
     
+    # Initialize click flags
+    if 'left_clicked' not in st.session_state:
+        st.session_state['left_clicked'] = False
+    if 'right_clicked' not in st.session_state:
+        st.session_state['right_clicked'] = False
+    
     title_img = 'images/generic/DnDRoster.webp'
  
     ########## MAIN ###########
@@ -39,29 +45,39 @@ def Roster():
     with lButton_col:
         lButton_absPath = get_absolute_path('images/generic/NavigatorButtonLeft.webp')
         left_button = get_image_as_base64(lButton_absPath)
-        clicked_left = clickable_images(
+        if clickable_images(
         [left_button],
         titles=["PREVIOUS IN ROSTER"],
         img_style={"height": "550px"},
         key='previous_roster_image_button'
-        )
+        ):
+            handle_left_click(portrait_files)
+
     # Right Navigation Button
     with rButton_col:
         rButton_absPath = get_absolute_path('images/generic/NavigatorButtonRight.webp')
         right_button = get_image_as_base64(rButton_absPath)
-        clicked_right = clickable_images(
+        if clickable_images(
         [right_button],
         titles=["NEXT IN ROSTER"],
         img_style={"height": "550px"},
         key='next_roster_image_button'
-        )
+        ): 
+            handle_right_click(portrait_files)
     
     st.divider()
     st.button("RETURN TO MENU", key="return_main_from_chargen_2", on_click=update_return_to_main_menu)
 
-    # Update the index based on button click
-    if clicked_left and st.session_state['current_portrait_index'] > 0:
-        st.session_state['current_portrait_index'] -= 1
+    if st.session_state['left_clicked']:
+        st.session_state['left_clicked'] = False
+    
+    if st.session_state['right_clicked']:
+        st.session_state['right_clicked'] = False
 
-    elif clicked_right and st.session_state['current_portrait_index'] < len(portrait_files) - 1:
-        st.session_state['current_portrait_index'] += 1
+def handle_left_click(portrait_files):
+    st.session_state['current_portrait_index'] = (st.session_state['current_portrait_index'] - 1) % len(portrait_files)
+    st.session_state['left_clicked'] = True
+
+def handle_right_click(portrait_files):
+    st.session_state['current_portrait_index'] = (st.session_state['current_portrait_index'] + 1) % len(portrait_files)
+    st.session_state['right_clicked'] = True
